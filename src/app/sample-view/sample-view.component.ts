464f4f7a3d71b011835a650d5b9ca83abf941b70
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-11-06 16:17:03 +0100

    Angular Core update

--- src/app/sample-view/sample-view.component.ts
+++ src/app/sample-view/sample-view.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sample-view',
-  standalone: true,
-  imports: [],
-  templateUrl: './sample-view.component.html',
-  styleUrl: './sample-view.component.css'
+    selector: 'app-sample-view',
+    templateUrl: './sample-view.component.html',
+    styleUrl: './sample-view.component.css'
 })
 export class SampleViewComponent {
 
