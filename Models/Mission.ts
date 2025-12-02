3152156f50b465a0c41073c44ce65976e0659019
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-12-02 09:41:44 +0100

    9411222025

--- Models/Mission.ts
+++ Models/Mission.ts
@@ -2,6 +2,14 @@ import { Agent } from "./Agent";
 
 export interface Mission{
     Id: number;
-    Name: String;
+    Name?: String;
+    Priority?: Number;
+    CreationDate?: Date;
+    Description?: String;
+    Tags?: String[];
+    Comments?: String[];
+    Attachments?: String[];
     AgentsAssigned?: Agent[];
 }
+
+
